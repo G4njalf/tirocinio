c16b8dea2630a8f3abc4963383359e6e8dba0136
G4njalf <federico.turrini@studio.unibo.it>
2025-04-29 13:49:04 +0200

    dadasd

--- backend/solidity/contracts/InsuranceFactory.sol
+++ backend/solidity/contracts/InsuranceFactory.sol
@@ -4,7 +4,12 @@ pragma solidity ^0.8.9;
 import "./InsuranceContract.sol";
 
 contract InsuranceFactory {
+    address public assicuratore;
     address[] public allInsuranceContracts;
+
+    constructor() {
+        assicuratore = msg.sender;
+    }
 
     function createInsurance(
         address payable assicurato,
@@ -13,7 +18,7 @@ contract InsuranceFactory {
         require(msg.value == premioAssicurativo, "Deposit mismatch");
         InsuranceContract newInsurance = (new InsuranceContract){
             value: msg.value
-        }(msg.sender, assicurato);
+        }(assicuratore, assicurato);
         allInsuranceContracts.push(address(newInsurance));
         return address(newInsurance);
     }
